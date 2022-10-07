858ca55337959268001c9ddaa05f99b56af77d32
khalid-aptech <khalid@aptechnorth.edu.pk>
2022-10-07 10:45:36 +0500

    add post

--- admin/add-products.php
+++ admin/add-products.php
@@ -1,36 +1,58 @@
 <?php include "header.php"; ?>
-  <div id="admin-content">
-      <div class="container">
-         <div class="row">
-             <div class="col-md-12">
-                 <h1 class="admin-heading">Add New Products</h1>
-             </div>
-              <div class="col-md-offset-3 col-md-6">
-                  <!-- Form -->
-                  <form  action="" method="POST" enctype="multipart/form-data">
-                      <div class="form-group">
-                          <label for="products_title">Title</label>
-                          <input type="text" name="products_title" class="form-control" autocomplete="off" required>
-                      </div>
-                      <div class="form-group">
-                          <label for="exampleInputPassword1"> Description</label>
-                          <textarea name="productsdesc" class="form-control" rows="5"  required></textarea>
-                      </div>
-                      <div class="form-group">
-                          <label for="exampleInputPassword1">Category</label>
-                          <select name="category" class="form-control">
-                              <option value="" selected> Select Category</option>
-                          </select>
-                      </div>
-                      <div class="form-group">
-                          <label for="exampleInputPassword1">Product image</label>
-                          <input type="file" name="fileToUpload" required>
-                      </div>
-                      <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
-                  </form>
-                  <!--/Form -->
-              </div>
-          </div>
-      </div>
-  </div>
+<div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="admin-heading">Add New Products</h1>
+            </div>
+            <div class="col-md-offset-3 col-md-6">
+                <!-- Form -->
+                <form action="save_post.php" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="products_title">Title</label>
+                        <input type="text" name="products_title" class="form-control" autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1"> Description</label>
+                        <textarea name="productsdesc" class="form-control" rows="5" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Category</label>
+                        <select name="category" class="form-control">
+
+                            <option value="" disabled> Select Category</option>
+
+                            <?php
+
+                            include "config.php";
+
+                            $query = "SELECT * FROM `category`";
+
+                            $result = mysqli_query($conn, $query);
+
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result))
+                                {
+                                    echo "<option value='{$row['category_id']}'>{$row['category_name']}</option>";
+                                }
+                            }
+
+
+
+
+
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Product image</label>
+                        <input type="file" name="fileToUpload" required>
+                    </div>
+                    <input type="submit" name="submit" class="btn btn-primary" value="Save" required />
+                </form>
+                <!--/Form -->
+            </div>
+        </div>
+    </div>
+</div>
 <?php include "footer.php"; ?>
---
+++ admin/save_post.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+include "config.php";
+
+
+if(isset($_FILES["fileToUpload"])){
+
+$error = array();
+
+$filename = $_FILES['fileToUpload']["name"];
+$filesize = $_FILES['fileToUpload']["size"];
+$file_temp = $_FILES['fileToUpload']["tmp_name"];
+$file_type = $_FILES['fileToUpload']["type"];
+$file_ext = explode('.',"$filename");
+$file_ext = end($file_ext);
+$file_ext = strtolower($file_ext);
+$extension = array("jpg","jpeg","png");
+
+if(in_array($file_ext,$extension) === false)
+{
+    $error[] = "File extension must be jpeg ,jpg ,png";
+}
+if($filesize > 2097152)
+{
+
+    $error[] =  "file is must be less than 2 mb";
+}
+if(empty($error)==true)
+{
+    move_uploaded_file($file_temp,"upload/".$filename);
+}
+}
+
+
+session_start();
+
+$title = $_POST["products_title"];
+$discription = $_POST["productsdesc"];
+$category = $_POST["category"];
+$date = date("d, M ,Y");
+$author = $_SESSION["user_id"];
+
+
+
+$query = "INSERT INTO `post`(`title`, `description`, `category`, `post_date`, `author`, `post_img`) VALUES ('{$title}','{$discription}',{$category},'{$date}','{$author}','{$filename}')";
+
+
+mysqli_query($conn, $query);
+
+
+
+?>
